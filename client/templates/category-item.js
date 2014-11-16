2cd17b64c382f6d55e1fa2c4be387a60eb5c1515
Yifei Yao <yao@Yifeis-MacBook-Pro.local>
2014-11-15 16:42:58 -0800

    commit temp: modified main page

---
+++ client/templates/category-item.js
@@ -0,0 +1,11 @@
+Template.categoryItem.helpers({
+  path: function () {
+    return Router.path('recipe', this.recipe);
+  },
+  
+  highlightedClass: function () {
+    if (this.size === 'large')
+      return 'highlighted';
+  },
+  
+});
